6b489f7f7d8abe5d7562f6a473ccfbac273b689b
NaimulSakib <naimulsakib.88@gmail.com>
2021-06-25 03:31:23 +0600

    changes

--- src/components/Login/firebase.config.js
+++ src/components/Login/firebase.config.js
@@ -1,9 +1,9 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyAzGsmyb5jaPH2spmKnFuFdudd_Zn8dTvA",
-  authDomain: "clean-bd-app1.firebaseapp.com",
-  projectId: "clean-bd-app1",
-  storageBucket: "clean-bd-app1.appspot.com",
-  messagingSenderId: "525541430481",
-  appId: "1:525541430481:web:f12b65f75e9893bd4121f0"
+  apiKey: "AIzaSyB_Z9T7HKSUz68oEr8hv7lFbHciAbe_qS0",
+  authDomain: "clean-bd-app2.firebaseapp.com",
+  projectId: "clean-bd-app2",
+  storageBucket: "clean-bd-app2.appspot.com",
+  messagingSenderId: "177078318884",
+  appId: "1:177078318884:web:0752b6de81a7f5f02f25b1"
 };
   export default firebaseConfig;
