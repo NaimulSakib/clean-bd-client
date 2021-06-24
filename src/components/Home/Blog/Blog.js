c641bc619636061edee994d26085fd868387033b
NaimulSakib <naimulsakib.88@gmail.com>
2021-06-25 02:47:16 +0600

    first commit

---
+++ src/components/Home/Blog/Blog.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import BlogPost from '../BlogPost/BlogPost';
+import blogImg1 from '../../../images/Blog1.jpg';
+import blogImg2 from '../../../images/blog2.jpg';
+import blogImg3 from '../../../images/blog3.jpg';
+import './Blog.css';
+
+const blogData = [
+    {
+        _id: 1,
+        title: 'Professional Carpet Cleaning',
+        date: '16 March, 2021',
+        post: 'Cleaning is the process of removing unwanted substances, such as dirt, infectious agents. Cleaning tasks such as dusting, mopping, waxing floors and vacuuming.',
+        image: blogImg1
+    },
+    {
+        _id: 2,
+        title: 'Confessions of an Office Cleaner ',
+        date: '09 June, 2021',
+        post: ' Allow yourself 2 1/2 hours a week to clean a two-bedroom home. You should also add an extra 1 hour a week when you decide to deep clean. If you have a typical',
+        image: blogImg2
+    },
+    {
+        _id: 3,
+        title: 'Ensure Good Hygiene at the Office',
+        date: '29 March, 2021',
+        post: 'Cleaning tasks such as dusting, sweeping floors and vacuuming. Refilling supplies, such as toilet paper. Cleaning spills, broken glass and other messes.',
+        image: blogImg3
+    }
+]
+
+const Blog = () => {
+    return (
+        <section className='blog-container mt-5 py-5'>
+            <div className="container">
+                <h1 className='text-center'>Cleaning Industry <span>News</span></h1>
+                <p className='text-center'>We write about industry developments, training, health and safety, eco-friendly cleaning products, recycling practices and advice for working with professional cleaners.</p>
+                <div className="blog-post">
+                    {
+                        blogData.map(blog => <BlogPost key={blog._id} blog={blog}></BlogPost>)
+                    }
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default Blog;
---
+++ src/components/Login/firebase.config.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAzGsmyb5jaPH2spmKnFuFdudd_Zn8dTvA",
+  authDomain: "clean-bd-app1.firebaseapp.com",
+  projectId: "clean-bd-app1",
+  storageBucket: "clean-bd-app1.appspot.com",
+  messagingSenderId: "525541430481",
+  appId: "1:525541430481:web:f12b65f75e9893bd4121f0"
+};
+  export default firebaseConfig;
